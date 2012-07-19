a405002f16607d2a8808a7cca83cdb815ddc092c
davherrmann <david.herrmann@fiducia.de>
2012-07-19 11:38:55 +0200

    added start_eclipse.bat

---
+++ start_eclipse.bat
@@ -0,0 +1,12 @@
+REM Author: David Herrmann
+REM 
+
+REM set environment variable for EGit to find the user's home directory
+setx HOME %USERPROFILE%
+
+REM set environment variable for Android Development Tools to find the .android folder in android-sdk
+setx ANDROID_SDK_HOME %CD%\android-sdk
+
+REM execute eclipse in the eclipse folder
+cd eclipse
+.\eclipse.exe
