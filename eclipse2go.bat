ca32b49fd6d3db17170f8a4b4d46543bcccf19d2
xce35d3 <xce35d3@C2264CE0.fidudom.fiducia.de>
2012-07-19 12:04:42 +0200

    removed setting HOME variable

--- eclipse2go.bat
+++ eclipse2go.bat
@@ -1,9 +1,6 @@
 REM Author: David Herrmann
 REM 
 
-REM set environment variable for EGit to find the user's home directory
-setx HOME %CD%\misc
-
 REM set environment variable for Android Development Tools to find the .android folder in android-sdk
 setx ANDROID_SDK_HOME %CD%\android-sdk
 
